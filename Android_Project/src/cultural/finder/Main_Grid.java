ef486efa2ea174543c1a14c0572715b16856b9b7
MarcObvious <cat166@gmail.com>
2014-09-03 18:00:37 +0000

    First aprox new classes

--- Android_Project/src/cultural/finder/Main_Grid.java
+++ Android_Project/src/cultural/finder/Main_Grid.java
@@ -4,18 +4,13 @@ import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.content.Context;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.TextView;
 
 public class Main_Grid extends Activity implements
 		NavigationDrawerFragment.NavigationDrawerCallbacks {
